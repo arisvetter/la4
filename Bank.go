88194cc25b949fcb490b9e0aab8fe098a19aa319
Aris Vetter <arisvetter@u.boisestate.edu>
2025-11-24 20:51:36 -1000

    mas

--- Bank.go
+++ Bank.go
@@ -45,7 +45,30 @@ func main() {
 	b := Bank{}
 	c := customer.NewCustomer("Ann")
 	b.Add(account.NewCheckingAccount("01001", 100.0, *c))
-	b.Add(account.NewSavingsAccount("02001", 200.0, *c))
+	b.Add(account.NewSavingsAccount("01002", 200.0, *c))
+	myChan := make(chan float64)
+	go b.Accrue(myChan, 0.02)
+	interest := <-myChan
+	fmt.Printf("Interest from Accruement:%.2f\n", interest)
+	// Please note: I am assuming we want to total the interest from the most recent accruement, not all time interest.
+	fmt.Printf(b.String())
+}
+
+func test() {
+	b := Bank{}
+	c := customer.NewCustomer("Ann")
+	b.Add(account.NewCheckingAccount("01001", 100.0, *c))
+	b.Add(account.NewSavingsAccount("01002", 200.0, *c))
+	b.Add(account.NewCheckingAccount("01003", 300.0, *c))
+	b.Add(account.NewSavingsAccount("01004", 400.0, *c))
+	b.Add(account.NewCheckingAccount("01005", 500.0, *c))
+	b.Add(account.NewSavingsAccount("01006", 600.0, *c))
+	b.Add(account.NewCheckingAccount("01007", 700.0, *c))
+	b.Add(account.NewSavingsAccount("01008", 800.0, *c))
+	b.Add(account.NewCheckingAccount("01009", 900.0, *c))
+	b.Add(account.NewSavingsAccount("01010", 1000.0, *c))
+	b.Add(account.NewCheckingAccount("01011", 1100.0, *c))
+	b.Add(account.NewSavingsAccount("010012", 1200.0, *c))
 	myChan := make(chan float64)
 	go b.Accrue(myChan, 0.02)
 	interest := <-myChan
